a4ec88569993bcf642364f8cede292baa941c420
Dhruvil Shah <65871885+Dhruvilshah007@users.noreply.github.com>
2021-11-10 22:06:35 +0530

    Update List.js

--- src/List.js
+++ src/List.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react";
+ 
 
+// here we are passing the people prop(state,variable containing data)
 const List = ({ people }) => {
   return (
     <>
       {people.map((person) => {
+
+        //destructuring the data variables,person is variable which will contain data one by one
         const { id, name, age, image } = person;
         return (
-          <article key={id} className='person'>
-            <img src={image} alt={name} />
+          <article key={id} className="person">
+            <img src={image} alt="" />
             <div>
               <h4>{name}</h4>
               <p>{age} years</p>
@@ -20,3 +24,8 @@ const List = ({ people }) => {
 };
 
 export default List;
+
+// here person is an argument which carries all values that is id,name,age
+// and image 
+// Map function always return new Array of same size
+// 
